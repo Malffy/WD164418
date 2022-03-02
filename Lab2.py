bb181bea8778d8137c0507b8dff18af3c269e439
Michał Pietrzak <92027641+Malffy@users.noreply.github.com>
2022-03-02 15:26:38 +0100

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,112 @@
+
+# a=5
+# b=7
+# if a>b:
+#     print("a jest wieksze od b")
+# elif a<b:
+#     print("a jest mniejsze od b")
+# else:
+#     print("liczby sa rowne")
+
+# a = input("wpisz liczbe:")
+# # print(a)
+# # print(type(a))
+# # a=int(a)
+# # print(type(a))
+
+# a=input("wpisz pierwsza liczbe: ")
+# b=input("wpisz druga liczbe: ")
+# c=input("wpisz trzecia liczbe: ")
+# d=input("wpisz czwarta liczbe: ")
+# a=int(a)
+# b=int(b)
+# c=int(c)
+# d=int(d)
+# if(a>b)&(c>d):
+#     print ("liczba a jest wieksza od liczby b i liczba c jest wieksza od liczby d")
+# else:
+#     print("liczba a jest mniejsza od liczby  b lub liczba c jest mniejsza od liczby d")
+
+# for i in range(1,7,1):
+#     print(i)
+# else:
+#     print("Petla wykonala sie.")
+
+# lista=['a',5,6,3.2,]
+# for i in lista:
+#     print(i)
+# else:
+#     print("wyswietlone zostaly wszystkie elementy z listy")
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+# else:
+#     print("Petla wykonala sie")
+
+# lista=[3,6,9,5,7,2,3]
+# liczba=input("podaj liczbe: ")
+# licznik=0
+# while licznik < len(lista):
+#     if int(liczba)- lista[licznik]==0:
+#         print('liczba'+str(liczba)+ '-' + str(lista[licznik])+" =0")
+#         break
+#     else:
+#         licznik +=1
+# else:
+#     print("Zadna z wartosci nie dala poprawnego wyniku")
+
+# lista1=[4,6,8,2,3,9]
+# lista2=[4,7,5,2]
+# suma=[]
+# for a in lista1:
+#     for b in lista2:
+#         wynik=a+b
+#         suma.append(wynik)
+#         print (suma)
+
+# a=input("Wczytaj pierwsza liczbe: ")
+# b=input("Wczytaj druga liczbe: ")
+# try:
+#     a=int(a)
+#     b=int(b)
+#     wynik=a/b
+#     print(wynik)
+# except ZeroDivisionError:
+#     print("Nie mozna dzielic przez 0")
+# except ValueError:
+#     print("Nie wczytano liczby calkowitej")
+
+# do listy append,insert,extend,pop,remove,del,sort,reverse
+lista=['a',5,5,5,[1,2,3]]
+slownik={"klucz":"wartosc","a":10}
+#a= lista[nr_indeksu]
+print (slownik)
+print(slownik["a"])
+#lista.reverse()
+#lista.append(5)
+#lista.insert(2,10)
+#lista.extend()
+#lista.remove("a")
+#lista.pop(0)
+
+
+print(lista)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
