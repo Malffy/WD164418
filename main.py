9658e68d8e755a4a9038a50faecb05c7e4ed065f
Michał Pietrzak <92027641+Malffy@users.noreply.github.com>
2022-04-27 15:24:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+import pandas as pd
+
+#Zadanie 1
+# a = pd.ExcelFile('imiona.xlsx')
+# df = pd.read_excel(a,header=0)
+# print(df)
+# b = df.groupby('Rok').agg({'Liczba':['sum']})
+# print(b)
+# c = df.Rok.unique()
+# b.plot(xticks=c,rot=90,fontsize=6)
+# plt.show()
+
+#Zadanie 2
